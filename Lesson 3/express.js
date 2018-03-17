6a7499a15a9d390e275d2c692e64d18a2de00258
David-Ter-Torosyan <davit.t.y@tumo.org>
2018-03-18 00:03:04 +0400

    rw90tgu0[3w

---
+++ Lesson 3/express.js
@@ -0,0 +1,14 @@
+/*var express = require("express");
+var app = express();
+
+app.get("/", function(req, res){
+   res.redirect("https://google.com");
+});
+app.get("/:name", function(req, res){
+   var name = req.params.name;
+   res.redirect("https://google.com/search?q="+name);
+});
+app.listen(3000, function(){
+   console.log("Example is running on port 3000");
+});
+*/
