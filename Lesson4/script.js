bd28c18164696be32cd3e6fb96dede651a9be0bd
erv17n <haycitrus@gmail.com>
2018-03-19 21:18:00 +0400

    Lesson5

--- Lesson4/script.js
+++ Lesson4/script.js
@@ -1,24 +1,14 @@
-function main() {
-    var socket = io.connect('http://localhost:3000');
-    var chatDiv = document.getElementById('chat');
-    var input = document.getElementById('message');
-    var button = document.getElementById('submit');
+var socket = io.connect();
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    background('#acacac');
+}
 
-    function handleSubmit(evt) {
-        var val = input.value;
-        if (val != "") {
-            socket.emit("send message", val);
-        }
-    }
-    button.onclick = handleSubmit;
-    function handleMessage(msg) {
-        var p = document.createElement('p');
-        p.innerText = msg;
-        chatDiv.appendChild(p);
-        input.value = "";
-    }
+function mouseDragged() {
+    socket.emit("send coords", [mouseX, mouseY]);
+}
 
-    socket.on('display message', handleMessage);
-} // main closing bracket
-
-window.onload = main;
+socket.on("display coords", function(data) {
+    fill('blue');
+    ellipse(data[0], data[1], 32, 32);
+})
